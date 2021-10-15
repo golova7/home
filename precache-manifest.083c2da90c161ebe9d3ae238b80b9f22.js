43613af93b2b000333d41a48c0f8093eb040c314
John Doe <johndoe@example.com>
2021-10-15 19:28:30 +0100

    Updates

---
+++ precache-manifest.083c2da90c161ebe9d3ae238b80b9f22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2402d6bbed7d95147f1727cce00ac547",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3859da1601e9d08070da",
+    "url": "/home/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "d7609a2711d976b150d4",
+    "url": "/home/static/js/2.e5029214.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.e5029214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3859da1601e9d08070da",
+    "url": "/home/static/js/main.71551048.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "0cd22610be564d3eac6b4205be22c093",
+    "url": "/home/static/media/carlo.0cd22610.png"
+  },
+  {
+    "revision": "957934b9b08178fe45d7e28fccf8c65c",
+    "url": "/home/static/media/freelancer.957934b9.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
